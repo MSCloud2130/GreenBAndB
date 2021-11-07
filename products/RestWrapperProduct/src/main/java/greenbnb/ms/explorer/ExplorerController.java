9ec47205db9e869f19a08b7140aae177d6749ac5
Jorge Salgado <salgadojorge@javeriana.edu.co>
2021-11-07 17:24:09 -0500

    Google maps api con loc quemada

--- products/RestWrapperProduct/src/main/java/greenbnb/ms/explorer/ExplorerController.java
+++ products/RestWrapperProduct/src/main/java/greenbnb/ms/explorer/ExplorerController.java
@@ -99,10 +99,104 @@ public class ExplorerController extends WebServiceGatewaySupport{
         return json;
 
     }
-    @PostMapping("/service")
+    @PostMapping("/")
     public Service createService(@RequestBody Service service ){
         System.out.println("POSTMAPPING");
         Service response = serviceservices.createService(service);
         return response;
+    }
+    @GetMapping("/location")
+    public String serviceLocation(){
+        String api_key = "AIzaSyBAfD2AfFCstxKHCuNsUJFzWDeoGhtaDNg";
+        String longitud =String.valueOf(-74.06483472329678);
+        String lat = String.valueOf(4.628477798453025) ;
+        String html = 
+"        <!DOCTYPE html>" +'\n' +
+
+"<html>" +'\n' +
+
+"  <head>" +'\n' +
+
+"    <title>Add Map</title>" +'\n' +
+
+
+"    <style type=\"text/css\">" + '\n' +
+
+"      /* Set the size of the div element that contains the map */"+ '\n' +
+
+"      #map {"+ '\n' +
+
+"        height: 400px;"+ '\n' +
+
+"        /* The height is 400 pixels */" +'\n' +
+
+"        width: 100%;" +'\n' +
+
+"        /* The width is the width of the web page */"+ '\n' +
+
+"      }"+ '\n' +
+
+"    </style>" +'\n' +
+
+"    <script>"+ '\n' +
+
+"      // Initialize and add the map"+ '\n' +
+
+"      function initMap() {"+ '\n' +
+
+"        // The location of Uluru" +'\n' +
+
+"        const uluru = { lat:"+lat+", lng: "+longitud  +" };"+ '\n' +
+
+"        // The map, centered at Uluru" +'\n' +
+
+"        const map = new google.maps.Map(document.getElementById(\"map\"), {"+ '\n' +
+
+"          zoom: 20,"+ '\n' +
+
+"          center: uluru," +'\n' +
+
+"        });" +'\n' +
+
+"        // The marker, positioned at Uluru" +'\n' +
+
+"        const marker = new google.maps.Marker({"+ '\n' +
+
+"          position: uluru,"+ '\n' +
+
+"          map: map," +'\n' +
+
+"        });"+ '\n' +
+
+"      }"+ '\n' +
+
+"    </script>" +'\n' +
+
+"  </head>"+ '\n' +
+
+"  <body>" +'\n' +
+
+"    <h3>My Google Maps Demo</h3>" +'\n' +
+
+"    <!--The div element for the map -->" +'\n' +
+
+"    <div id=\"map\"></div>" +'\n' +
+
+
+"    <!-- Async script executes immediately and must be after any DOM elements used in callback. -->"+ '\n' +
+
+"    <script"+ '\n' +
+
+"      src=\"https://maps.googleapis.com/maps/api/js?key="+api_key+"&callback=initMap&libraries=&v=weekly\"" +'\n' +
+
+"      async" +'\n' +
+
+"    ></script>" +'\n' +
+
+"  </body>" +'\n' +
+
+"</html>" +'\n';
+
+        return html;
     }
 }
