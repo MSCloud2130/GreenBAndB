ce9add2ea23ff8c7a5fba4e49dd49608ba7c81e1
NicolasCamachoP <nicolas-camacho@javeriana.edu.co>
2021-11-10 01:28:53 +0000

    Devops Finished
    Co-authored-by: Mateo Florido <mateoflosan@gmail.com>

---
+++ products/RestWrapperProduct/src/main/java/greenbnb/ms/explorer/ServletInitializer.java
@@ -0,0 +1,13 @@
+package greenbnb.ms.explorer;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExplorerApplication.class);
+	}
+
+}
